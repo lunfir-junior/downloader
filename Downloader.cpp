bf84e15a2b6eaf294f29a28f660971870a41a138
Lunfir <lunfirok@gmail.com>
2018-02-26 20:35:17 +0200

    before change logic

--- Downloader.cpp
+++ Downloader.cpp
@@ -2,15 +2,19 @@
 
 Downloader::Downloader(QObject *parent) : QObject(parent)
 {
-  m_networkAccess = new QNetworkAccessManager(this);
+  m_manager = new QNetworkAccessManager(this);
   m_notifier = new QSocketNotifier(fileno(stdin), QSocketNotifier::Read, this);
+  m_download = nullptr;
 
   connect(m_notifier, &QSocketNotifier::activated, this, &Downloader::getUrl);
+
+  connect(this, &Downloader::signalCanStartDownload, this, &Downloader::startDownload);
+  connect(this, &Downloader::signalCanProcessDownload, this, &Downloader::processDownload);
 }
 
 Downloader::~Downloader()
 {
-  delete m_networkAccess;
+  delete m_manager;
 }
 
 void Downloader::getUrl()
@@ -18,12 +22,53 @@ void Downloader::getUrl()
   qDebug() << "in getUrl slot";
 
   std::string line;
-//  QString tmp;
+  QString tmp;
   QUrl url;
 
   std::getline(std::cin, line);
-//  tmp = QString
-  url = QUrl(QString::fromStdString(line));
+  tmp = QString::fromStdString(line);
+  url = QUrl(tmp);
+  m_filename = tmp.split('/', QString::SkipEmptyParts, Qt::CaseSensitive).takeLast();
+  qDebug() << "m_filename: " << m_filename;
 
-  qDebug() << "url: " << url;
+  emit signalCanStartDownload(url);
+}
+
+void Downloader::startDownload(const QUrl &url)
+{
+  qDebug() << "in startDownload slot";
+  qDebug() << url;
+
+  QNetworkRequest request = QNetworkRequest(url);
+
+  m_download = m_manager->get(request);
+
+  emit signalCanProcessDownload(/*m_download*/);
+}
+
+void Downloader::processDownload(/*QNetworkReply *reply*/)
+{
+  qDebug() << "in processDownload slot";
+  QUrl url = m_download->url();
+  qDebug() << url;
+
+  QFile m_file(m_filename);
+
+    if (!m_file.open(QIODevice::WriteOnly)) {
+        fprintf(stderr, "Could not open %s for writing: %s\n",
+                qPrintable(m_filename),
+                qPrintable(m_file.errorString()));
+//        return false;
+    }
+
+    m_file.write(m_download->readAll());
+    m_file.close();
+
+//    return true;
+
+}
+
+void Downloader::finishDownload()
+{
+
 }
--- Downloader.h
+++ Downloader.h
@@ -3,7 +3,10 @@
 
 #include <QObject>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
 #include <QSocketNotifier>
+#include <QFile>
+#include <QString>
 #include <iostream>
 
 class Downloader : public QObject
@@ -14,13 +17,23 @@ public:
   ~Downloader();
 
 private:
-  QNetworkAccessManager *m_networkAccess;
+  QNetworkAccessManager *m_manager;
+  QNetworkReply *m_download;
   QSocketNotifier *m_notifier;
+//  QFile m_file;
+  QString m_filename;
+
+//  bool saveFile();
 
 signals:
+  void signalCanStartDownload(const QUrl &url);
+  void signalCanProcessDownload(/*QNetworkReply *reply*/);
 
 public slots:
   void getUrl();
+  void startDownload(const QUrl &url);
+  void processDownload();
+  void finishDownload();
 };
 
 #endif // DOWNLOADER_H
